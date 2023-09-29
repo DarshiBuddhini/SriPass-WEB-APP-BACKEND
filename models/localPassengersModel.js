1e2e12baf8c1be3f50140b6dec40ab317540aa79
Imesh Pasinda <imeshpasinda@gmail.com>
2023-09-29 09:06:51 +0530

    Update localPassengersModel.js

--- models/localPassengersModel.js
+++ models/localPassengersModel.js
@@ -34,6 +34,14 @@ const localPassengersSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
   // Add other fields as needed
 });
 
