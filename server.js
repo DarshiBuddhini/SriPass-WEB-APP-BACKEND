969ed5f357c41bbd7b524c335712ff3b2880b889
Imesh Pasinda <imeshpasinda@gmail.com>
2023-10-08 11:06:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -43,321 +43,27 @@ app.get("/", (req, res) => {
     `).join('');
 
     const mitLicenseText = `
-    ---Question 1-----
+    MIT License
 
-CREATE OR REPLACE TYPE Empl_typ AS OBJECT 
-(
- empNo INT,
- empName VARCHAR2(50),
- salary FLOAT,
- designation VARCHAR2(50)
-);
-/
-
-CREATE OR REPLACE TYPE Member_typ AS OBJECT
-(
- team_member REF Empl_typ
-);
-/
-
-CREATE TYPE Member_nt AS TABLE OF Member_typ;
-/
-
-CREATE TYPE Project_typ AS OBJECT
-(
- projNo INT,
- pname VARCHAR2(50),
- members Member_nt,
- mgr REF Empl_typ
-);
-/
-
-CREATE TABLE Employees OF Empl_typ(PRIMARY KEY(empNo));
-
-CREATE TABLE Projects OF Project_typ(PRIMARY KEY (projNo), mgr REFERENCES Employees)
-NESTED TABLE members STORE AS member_tab;
-
-INSERT INTO Employees VALUES (1, 'Sarath', 10000.0, 'Technician');
-INSERT INTO Employees VALUES (2, 'Sampath', 12000.0, 'Sales Officer');
-INSERT INTO Employees VALUES (3, 'Viraj', 18000.0, 'Software Developer');
-INSERT INTO Employees VALUES (4, 'Tusith', 22000.0, 'Director');
-INSERT INTO Employees VALUES (5, 'Nimali', 15000.0, 'Technician');
-
-INSERT INTO Projects VALUES (10, 'Cabling Project', 
-member_nt(
-Member_typ((select ref(e) from Employees e where e.empNo=1)),
-Member_typ((select ref(e) from Employees e where e.empNo=5))),  
-null);
- 
-INSERT INTO Projects VALUES (12, 'Data Warehousing Proj', 
-member_nt(
-Member_typ((select ref(e) from Employees e where e.empNo=2)),
-Member_typ((select ref(e) from Employees e where e.empNo=3)),
-Member_typ((select ref(e) from Employees e where e.empNo=4))),  
-(select ref(e) from Employees e where e.empNo=2)
-);
-
-
-Consider the database script is given. Execute it to create the appropriate schema required for the practical test.
-
-Write Oracle’s Object SQL statements to the following:
-
----------1---------
-Print the project number, project name and project manager’s salary.  The results should be printed is descending order of salary values.
-
-SELECT p.projNo, p.pname, m.team_member.salary
-FROM Projects p
-JOIN Employees m ON p.mgr = REF(m)
-ORDER BY m.salary DESC;
-
----------2---------
-Write a member method (called Budget) that finds returns the total salary of all members in the project.
-
-CREATE OR REPLACE TYPE Project_typ AS OBJECT
-(
-  projNo INT,
-  pname VARCHAR2(50),
-  members Member_nt,
-  mgr REF Empl_typ,
-  
-  MEMBER FUNCTION Budget RETURN FLOAT
-);
-/
-
-
-CREATE OR REPLACE TYPE BODY Project_typ AS
-  MEMBER FUNCTION Budget RETURN FLOAT IS
-    totalSalary FLOAT := 0.0;
-  BEGIN
-    FOR i IN 1..self.members.COUNT LOOP
-      totalSalary := totalSalary + self.members(i).team_member.empNo.salary;
-    END LOOP;
+    Copyright (c) 2023 SriPass
     
-    RETURN totalSalary;
-  END;
-END;
-/
-
-
-
----------3----------
-Use the method created above to find the project with the least budget greater than 20000. Print the projNo.
-
-SELECT projNo
-FROM Projects
-WHERE Budget() > 20000
-ORDER BY Budget()
-FETCH FIRST 1 ROW ONLY;
-
-
-
------Question 2-----
-
-create type account_type_typ as object (
- code char(4),
- name varchar2(50),
- interest float,
- min_bal float
-);
-/
-
-create type account_typ as object (
- account_no char(10),
- account_ty ref account_type_typ,
- balance float
-);
-/
-
-create type account_tlb as table of account_typ;
-/
-
-create table account_types of account_type_typ (code primary key);
-
-create type customer_typ as object (
- id char(10),
- name varchar2(50),
- accounts account_tlb
-);
-/
-
-create table customers of customer_typ(id primary key)
- nested table accounts store as ntlb_accounts;
-
-insert into account_types values ('SAVS', 'General Savings', 5.24, 100.0);
-insert into account_types values ('CHEQ', 'General Checking', 0.0, 0);
-insert into account_types values ('HAPS', 'Hapan Savings', 6.26, 20000.0);
-
-
-insert into customers values (
- '781250401V',
- 'Sampath Weerasinghe',
- account_tlb(
-  account_typ('1122300004',
-            (select ref(s) from account_types s where s.code = 'SAVS'), 5700.00),
-  account_typ('2334453124',
-            (select ref(s) from account_types s where s.code = 'CHEQ'), 2300.00)
- )
-);
-
-insert into customers values (
- customer_typ (
-  '99334453V', 'Dulani Pieris',
-  account_tlb(
-   account_typ('3445663321',
-            (select ref(s) from account_types s where s.code = 'HAPS'), 19045.06),
-   account_typ('5000022123',
-            (select ref(s) from account_types s where s.code = 'SAVS'), 55235.00)
-  )
- )
-);
-
-
-1)Deposit Rs. 1200/- to the account (Acc # 1122300004) of Mr. Sampath Weerasignhe (ID # 781250401V)
-
-UPDATE TABLE(SELECT c.accounts FROM customers c WHERE c.id = '781250401V') a
-SET a.balance = a.balance + 1200
-WHERE a.account_no = '1122300004';
-
-2)Write a member function (called TotBal) that returns the total balance of a customer’s accounts.
-
-CREATE OR REPLACE TYPE customer_typ AS OBJECT (
-  id CHAR(10),
-  name VARCHAR2(50),
-  accounts account_tlb,
-  MEMBER FUNCTION TotBal RETURN FLOAT
-);
-CREATE OR REPLACE TYPE BODY customer_typ AS
-  MEMBER FUNCTION TotBal RETURN FLOAT IS
-    total_balance FLOAT := 0;
-  BEGIN
-    FOR i IN 1..self.accounts.COUNT LOOP
-      total_balance := total_balance + self.accounts(i).balance;
-    END LOOP;
-    RETURN total_balance;
-  END TotBal;
-END;
-/
-
-3)Write a query to print the names of customers and their total balances (using the function created in question 2)
-
-SELECT c.name, c.accounts.TotBal() AS total_balance
-FROM customers c;
-
-///////////////////////////////////////////////////////////////////////
-
-CREATE TYPE type_name;
-
-
-
-CREATE TYPE type_name AS OBJECT(
-
-company REF stock_t,
-
-);
-
-
-
-CREATE TABLE table_name OF type_name ();
-
-
-CREATE TYPE nestedTbl_t as TABLE OF in nested_t;
-
-
-
-CREATE TABLE table_name(
-XXXXX
-XXXXX
-XXXXX
-investments investmentNestedTbl_t,
-constraint pk_clientNo primary key (clientNumber)
-)
-NESTED TABLE investments STORE AS Investments
-
-
-
-
-CREATE TYPE exchanges_varray as VARRAY(3) of VARCHAR2(40)
-
-
-SELECT c.fname || '' || c.lname AS client_name,
-	i.company.company AS stock_name,
-	SUM(i.qty) AS total_shares_held,
-	SUM(i.purchasePrice*i.qty)/SUM(i.qty)AS average_purchase_price
-FROM Clients c
-CROSS JOIN TABLE (c.investments)i
-GROUP BY c.fname,c.lname,i.company.company;
-
-
-
-ALTER TYPE stock_t
-ADD MEMBER FUNCTION computeYield
-RETURN FLOAT CASCADE;
-/
-
-CREATE OR REPLACE TYPE BODY stock_t AS
-MEMBER FUNCTION computeYield RETURN FLOAT IS
-    BEGIN
-        RETURN SELF.lastdividend / SELF.currprice*100;
-    END computeYield;
-END;
-/
-
-
-
-ALTER TYPE stock_t
-ADD MEMBER FUNCTION priceInUSD(rate FLOAT)
-RETURN FLOAT CASCADE;
-
-
-
-CREATE OR REPLACE TYPE BODY stock_t AS
-MEMBER FUNCTION computeYield RETURN FLOAT IS
-    BEGIN
-        RETURN SELF.lastdividend / SELF.currentprice*100;
-    END computeYield;
-MEMBER FUNCTION priceInUSD(rate FLOAT) RETURN FLOAT IS
-    BEGIN
-        RETURN rate*SELF.currentprice;
-    END priceInUSD;
-END;
-/
-
-
-
-
-CREATE TABLE AdminDocs(
-   id int primary key,
-   xDoc XML not null
-)
-
-INSERT INTO AdminDocs VALUES (1,
-'<book >
-        <title>Writing Secure Code</title>
-        <author>
-        <first-name>Michael</first-name>
-         <last-name>Howard</last-name>
-        </author>
-        <author>
-          <first-name>David</first-name>
-          <last-name>LeBlanc</last-name>
-        </author>
-        <price>39.99</price>
-</book>'
-)
-
-
-SELECT id exml.query('/Persons/Person/Name')
-SELECT id exml.query('/Persons/Person[@id = "1"]')
-SELECT id exml.query('decendant-or-self::Person[@id = "1"]')
-SELECT id exml.query('/Persons/child::Person[attribute::id = "1"]')
-SELECT id exml.query('/Persons/Person/Age. gt 20[]')
-SELECT id exml.query('for $person in /Persons/Person
-                      let $x := $person/Age
-					  where $x > 23
-					  return $$person')
-FROM demo
-WHERE id = 1
-
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
     
     `;
 
@@ -402,10 +108,7 @@ WHERE id = 1
                     white-space: pre-line;
                     font-size: 12px;
                     color: #777;
-                    text-align: left; 
-                    width: 100%; 
                 }
-                
                 .logo {
                     display: block;
                     margin: 0 auto;
@@ -418,11 +121,17 @@ WHERE id = 1
         </head>
         <body>
             <div class="container">
-                         
+                <img class="logo" src="https://github.com/SriPass/SriPass-WEB-APP-BACKEND/blob/main/logo.png?raw=true" alt="Logo">
+                <div class="title">SriPass Backend Server (c) 2023 SriPass</div>
+                <div class="status">Active</div>
+                <h5>Code Authors</h5>
+                <div class="authors">
+                    ${authorList}
+                </div>
                 <div class="license">
                     ${mitLicenseText}
                 </div>
-                
+                <p class="repo-link">GitHub Repository: <a href="https://github.com/SriPass/SriPass-WEB-APP-BACKEND" target="_blank">https://github.com/SriPass/SriPass-WEB-APP-BACKEND</a></p>
             </div>
         </body>
         </html>
